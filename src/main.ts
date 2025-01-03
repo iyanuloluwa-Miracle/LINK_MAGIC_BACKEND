c47dd8ee4c5820839483b296ea1e7face0a0cdd2
iyanuloluwa-Miracle <iyanudina@gmail.com>
2025-01-03 20:52:45 +0100

    feat:added CORS support to backend for frontend communication

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN || 'https://link-magic.vercel.app/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
